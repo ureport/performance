1218e55dad1c9472e19e611557e2103e8f92e6c3
Jim Barritt <jim@planet-ix.com>
2013-04-23 09:06:27 +0300

    [jmdb] adding basic create poll test

--- massive-poll/celery_exec.py
+++ massive-poll/celery_exec.py
@@ -1,9 +1,10 @@
+import time
+
 def execute_async(func):
     print "Executing [" + func.name + "] Asynchronously..."
     result = func.delay()
     while result.ready() != True:
         print "Waiting 0.5 secs for a result..."
-        import time
         time.sleep(0.5)
     print "Got a result!"
     print "Result was [" + str(result.info) + "]"
--- massive-poll/create-massive-poll.py
+++ massive-poll/create-massive-poll.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
-import ureport_env, os
+import ureport_env, os, time, uuid
 from celery_exec import execute_async
+from splinter import Browser
 
+with Browser() as browser: 
+     # Visit URL 
+     url = "http://localhost:8088/createpoll/" 
+     browser.visit(url) 
+
+     browser.fill("username", "functest")
+     browser.fill("password", "functest")
+     browser.find_by_css("input[type=submit]").first.click()
+
+     pollName = "PerfTestPoll-" + str(uuid.uuid4())
+     print "Created poll called " + pollName                               
+     browser.fill("name", pollName)
+     browser.fill("question_en", "Will this poll work?")
+     browser.select("groups", "54")
+     browser.find_link_by_href("javascript:void(0);").click()
+     time.sleep(10)
 
 
 
