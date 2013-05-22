1e6c9c67f9adae173a39245daa5f2d31d31e2894
wanderanimrod <wanderanimrod@gmail.com>
2013-05-22 16:10:35 +0300

    [nim, jmdb] Added functionality to set up data for categorical polls

--- massive-poll/setup_geoserver_data.py
+++ massive-poll/setup_geoserver_data.py
@@ -7,7 +7,7 @@ from ureport.tasks import ping
 
 print "Ok, going to do something...."
 
-from geoserver.models import PollData
+from geoserver.models import PollData, PollCategoryData
 
 pd, _ = PollData.objects.using('geoserver').get_or_create(\
                             district="Fashooda",\
@@ -17,4 +17,23 @@ pd.yes=.75
 pd.no=.15
 pd.uncategorized=.5
 pd.unknown=.5
+pd.save()
+
+category_names = ["A", "B", "C"]
+
+values = {
+    'A': .2,
+    'B': .6,
+    'C': .2
+}
+
+description = "<br/>".join(["%s: %0.1f%%" % (cat_name, (values[cat_name] * 100)) for cat_name in category_names])
+
+pd, _ = PollCategoryData.objects.using('geoserver').get_or_create(
+    district="Fashooda",
+    poll_id=281,
+    deployment_id=1
+)
+pd.description = description
+pd.top_category = 1
 pd.save()
